99e9188df68c4af8164aa1c50d3c762387270abd
strujilloc <3881022596@edu.k.u-tokyo.ac.jp>
2020-03-13 21:04:31 +0900

    create geojson schema

--- models/ninja.js
+++ models/ninja.js
@@ -1,6 +1,17 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+//Create geolocation Schema
+const GeoSchema = new Schema({
+  type: {
+    type: String,
+    default: "Point"
+  },
+  coordinates: {
+    type: [Number],
+    index: "2dsphere"
+  }
+});
 //create ninja Schema & model
 const NinjaSchema = new Schema({
   name: {
@@ -13,7 +24,8 @@ const NinjaSchema = new Schema({
   available: {
     type: Boolean,
     default: false
-  }
+  },
+  geometry: GeoSchema
   //add geo location
 });
 
