5c0c9d9a4b49dfbbaa8560fcc113e1d334f8885d
strujilloc <3881022596@edu.k.u-tokyo.ac.jp>
2020-03-13 17:09:30 +0900

    set get method

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const express = require("express");
 // set up express app
 const app = express();
 
-// listen for requests
+//req = request res= respond
+app.get("/api", (req, res) => {
+  console.log("Get request");
+  res.send({ name: "Yoshi" });
+});
+
+// listen for requests process.env.port explain in video #4
 app.listen(process.env.PORT || 4000, () => {
   console.log("now listening for requests");
 });
